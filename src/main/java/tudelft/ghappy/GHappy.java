dc6c225b220341f5f331dd77c07e29a2906453c5
Minenco Daria <daria.minenco.1504@gmail.com>
2025-05-25 21:41:02 +0300

    Fixed gHappy logic and added tests

--- src/main/java/tudelft/ghappy/GHappy.java
+++ src/main/java/tudelft/ghappy/GHappy.java
@@ -6,9 +6,12 @@ public class GHappy {
         assert str!=null;
         for(int i = 0; i < str.length(); i++) {
             if(str.charAt(i) == 'g') {
-                if (i >= 0 && str.charAt(i-1) == 'g') { continue; }
-                if (i+1 < str.length() && str.charAt(i+1) == 'g') { continue; }
-                return false;
+               boolean hasLeftG = i>0 && str.charAt(i-1) == 'g';
+               boolean hasRightG = i +1 < str.length() && str.charAt(i+1) == 'g';
+                if(!hasRightG && !hasLeftG)
+                {
+                    return false;
+                }
             }
         }
 
--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,40 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+@Test
+    public void testHappyGG(){
+    GHappy gh = new GHappy();
+    Assertions.assertTrue(gh.gHappy("xxggxx"));
+
 }
+
+@Test
+    public void testLonelyG(){
+    GHappy gh = new GHappy();
+    Assertions.assertFalse(gh.gHappy("xxgxx"));
+}
+
+
+@Test
+    public void testLastGIsLonely(){
+    GHappy gh = new GHappy();
+    Assertions.assertFalse(gh.gHappy("xxggyygxx"));
+}
+
+@Test
+    public void testEmptyString(){
+    GHappy gh = new GHappy();
+    Assertions.assertTrue(gh.gHappy(""));
+}
+
+@Test
+    public void testNoGAtAll(){
+    GHappy gh = new GHappy();
+    Assertions.assertTrue(gh.gHappy("absdef"));
+}
+
+}
+
